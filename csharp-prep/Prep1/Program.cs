8e06e13283797ee15aeaff36515ac796cbee253e
lilwoodsgh <patamia16@gmail.com>
2024-04-29 21:49:26 +0000

    done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Kindly Input Your Firstname:");
+        string fname = Console.ReadLine();
+
+        Console.Write("Kindly Input Your Lastname:");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {fname} {lname}.");
     }
 }
