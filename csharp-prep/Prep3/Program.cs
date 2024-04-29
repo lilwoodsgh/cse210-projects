9a5a81d06f621abb90225e01954e821f9f22095a
lilwoodsgh <patamia16@gmail.com>
2024-04-29 23:14:42 +0000

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // user guessing a specified number
+        Console.WriteLine("Kindly Guess the Magic Number:");
+        int MagicNumber = int.Parse(Console.ReadLine());
+        int Guess = 20;
+
+        while (Guess != MagicNumber)
+        {
+            Console.Write("Kindly Guess Again:");
+            Guess = int.Parse(Console.ReadLine());
+
+            if (MagicNumber > Guess)
+            {
+                Console.WriteLine("Too High, Guess a Lower number");
+            }
+            else if (MagicNumber < Guess)
+            {
+                Console.WriteLine("Too Low, Guess a Higher Number");
+            }
+             else if (MagicNumber == Guess)
+            {
+                Console.WriteLine("Too Low, Guess a Higher Number");
+            } 
+        }
+        Console.WriteLine("Congratulations,You Won!!");
+        
     }
 }
