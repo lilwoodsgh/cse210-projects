38418a88452529572e476092935a0df303d74b75
lilwoodsgh <patamia16@gmail.com>
2024-04-29 22:20:23 +0000

    done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Data.Common;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Kindly insert your Score out of 100: ");
+        string answer = Console.ReadLine();
+        int marks = int.Parse(answer);
+
+        string grade = "";
+
+        if (marks >= 90)
+         {
+            grade = "A";
+         } 
+         else if ( marks >= 80)
+         {
+            grade = " B ";
+         }
+         else if ( marks >= 70)
+         {
+            grade = "C";
+         } else if ( marks >= 60)
+         {
+            grade = " D ";
+         }
+         else if ( marks >= 50)
+         {
+            grade = " E ";
+         } 
+         else 
+         {
+            grade = " F ";
+         }
+         
+         Console.WriteLine($"Your Grade is: {grade}");
+
+         if (marks >=70)
+         {
+            Console.WriteLine("Hurray You Passed!");
+         }
+         else
+         {
+            Console.WriteLine("Sorry, Better Luck Next Time.");
+         }
+         } 
     }
 }
