17ccc8eb979969a426d20186abd28f46be901726
lilwoodsgh <patamia16@gmail.com>
2024-04-30 00:05:45 +0000

    tired

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,30 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        // user guessing a specified number
-        Console.WriteLine("Kindly Guess the Magic Number:");
-        int MagicNumber = int.Parse(Console.ReadLine());
-        int Guess = 20;
+        bool playAgain = true;
 
-        while (Guess != MagicNumber)
+        while (playAgain)
         {
-            Console.Write("Kindly Guess Again:");
-            Guess = int.Parse(Console.ReadLine());
+            // Generate a random number from 1 to 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
 
-            if (MagicNumber > Guess)
+            bool guessedCorrectly = false;
+            int guesses = 0;
+
+            // Game loop
+            while (!guessedCorrectly)
             {
-                Console.WriteLine("Too High, Guess a Lower number");
+                // Ask the user for their guess
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                // Check if the guess matches the magic number
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessedCorrectly = true;
+                }
             }
-            else if (MagicNumber < Guess)
+
+            Console.WriteLine("It took you " + guesses + " guesses.");
+
+            // Ask the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainResponse = Console.ReadLine();
+
+            if (playAgainResponse.ToLower() != "yes")
             {
-                Console.WriteLine("Too Low, Guess a Higher Number");
+                playAgain = false;
             }
-             else if (MagicNumber == Guess)
-            {
-                Console.WriteLine("Too Low, Guess a Higher Number");
-            } 
         }
-        Console.WriteLine("Congratulations,You Won!!");
-        
     }
 }
